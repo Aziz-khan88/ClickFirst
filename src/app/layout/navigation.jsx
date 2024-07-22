802ef924075cd4dcb6bc3fdac41d62f949b3d53a
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-07-22 23:53:57 +0500

    submenu Added in header

--- src/app/layout/navigation.jsx
+++ src/app/layout/navigation.jsx
@@ -1,15 +1,47 @@
 import Link from 'next/link'
 import styles from "../../../styles/layout/navigation.module.scss"
+import { MenuIcon, SubMenuIcon } from '../app-constants'
+
 
 const Navigation = (props) => {
+
     return (
         <div className={`${styles.mainMenu} ${props.isActive ? `${styles.active}` : ''}`}>
             <ul>
                 <li>
                     <Link href="#">Home</Link>
                 </li>
-                <li>
-                    <Link href="#">Services</Link>
+                <li >
+                    <Link href="#" >Services <SubMenuIcon /></Link>
+                    <div className={styles.subMenu} >
+                        <div className={styles.MenuListing}>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />Social Media Management</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />Social Media Marketing</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />Instagram Marketing</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />ClickFirstSMM Tiktok Marketing</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />Social Media Advertising</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />Facebook Marketing</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon />Influencers Marketing</Link>
+                            </div>
+                            <div className={styles.MenuList}>
+                                <Link href="#"><MenuIcon /> Snapchat Marketing</Link>
+                            </div>
+                        </div>
+                    </div>
+
                 </li>
                 <li>
                     <Link href="#">Influencer Marketing</Link>
